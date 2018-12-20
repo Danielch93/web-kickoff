9d2772d1f33d4e290b2e8149ac4002bed51d8720
Daniel Charry <daniel.charry@wolox.co>
2018-12-20 14:26:35 -0500

    Created rules of the linters and files are cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
-     <h1>Hola Web Kickoff</h1>
-    );
+      <div>
+        <h1>Hola Web Kickoff</h1>
+      </div>
+
+    )
   }
 }
 
-export default App;
+export default App
